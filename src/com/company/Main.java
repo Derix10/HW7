3f61d39859dc6636a83a3afd9f78a73cf958bfbf
Derix10 <sejitbekovarsen12.@gmail.com>
2022-08-03 08:42:58 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Medic medic = new Medic();
+        Magic magic = new Magic();
+        Warrior warrior = new Warrior();
+        Hero[] myheroes = {medic,magic, warrior};
+        for (int i = 0; i < myheroes.length; i++) {
+             if (myheroes[i] instanceof Medic){
+                 medic.applySuperAbility("Cure everyone");
+             }
+            if (myheroes[i] instanceof Magic){
+                magic.applySuperAbility("Fireball");
+            }
+            if (myheroes[i] instanceof Warrior){
+                warrior.applySuperAbility("Physical damage");
+            }
+        }
+    }
+}
